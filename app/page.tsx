4c85cfe81db91321f0cefa3b4bbd68be5aa6d4c0
SantoshYadav22 <santoshbusiness108@gmail.com>
2024-01-13 13:04:45 +0530

    list

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,32 @@
-import User from './User'
 
-export default function Home() {
+var fname: string[] = [];
+const Home = () => {
+  let callAPI = async () => {
+    try {
+      let res = await fetch('http://127.0.0.1:8000/api/list');
+      let data = await res.json();
+
+      for (var x in data) {
+        fname[+x]  = data[x].prod_name;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+    callAPI();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>sanotsh</h2>
-      <User />
-      <Apps name="developer" />
+    <div className=''>
+      <main className=''>
+       
+      <ul>
+        {fname.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>      </main>
+    </div>
+  );
+};
 
-    </main>
-  )
-}
-
-const Apps = (props:any) =>{
-  return(
-    <div>  {props.name}</div>
-  )
-}
+export default Home;
